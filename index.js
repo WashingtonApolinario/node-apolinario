794279ccaa299ee12bbfa7ba51d34007213d805d
WashingtonApolinario <dekontruccion@gmail.com>
2018-08-21 10:53:37 -0500

    primer commit

---
+++ index.js
@@ -0,0 +1,22 @@
+
+const express = require('express');
+const path  = require('path');
+const PORT = process.env.PORT || 5000;
+
+var exphbs = require('express-handlebars');
+var app = express();
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.get('/', function (req, res) {
+    res.render('home');
+});
+
+app.get('/', (req, res) => res.render('pages/index'));
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
+// app.set('port', process.env.PORT || 3000);
+//
+// app.listen(app.get('port'), function(){
+// 	console.log('express on localhost ' + app.get('port'));
+// });
